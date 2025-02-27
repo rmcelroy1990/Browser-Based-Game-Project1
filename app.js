1e8da569bdb18e7bde6eacacd996e72ae7498eeb
rmcelroy1990 <mcelroy.raeme@gmail.com>
2025-02-27 14:05:04 -0800

    project1

---
+++ app.js
@@ -0,0 +1,57 @@
+const gameBoard = document.getElementById('game-board');
+const cardsData = ['A', 'B', 'C', 'D', 'A', 'B', 'C', 'D'];
+let flippedCards = [];
+let matchedCards = [];
+
+function shuffleCards(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+function createCard(value) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.dataset.value = value;
+    card.addEventListener('click', flipCard);
+    return card;
+}
+
+function flipCard() {
+    if (flippedCards.length < 2 && !flippedCards.includes(this) && !matchedCards.includes(this)) {
+        this.classList.add('flipped');
+        flippedCards.push(this);
+
+        if (flippedCards.length === 2) {
+            checkForMatch();
+        }
+    }
+}
+
+function checkForMatch() {
+    const [card1, card2] = flippedCards;
+    if (card1.dataset.value === card2.dataset.value) {
+        matchedCards.push(card1, card2);
+        flippedCards = [];
+        if (matchedCards.length === cardsData.length) {
+            alert('You win!');
+        }
+    } else {
+        setTimeout(() => {
+            card1.classList.remove('flipped');
+            card2.classList.remove('flipped');
+            flippedCards = [];
+        }, 1000);
+    }
+}
+
+function startGame() {
+    shuffleCards(cardsData);
+    cardsData.forEach(value => {
+        const card = createCard(value);
+        gameBoard.appendChild(card);
+    });
+}
+
+startGame();
